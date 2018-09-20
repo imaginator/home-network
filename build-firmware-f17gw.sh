37a703f30f798d439cb641272d103f966e5addf0
Simon Tennant <simon@imaginator.com>
2018-09-20 16:14:23 +0000

    f17-gateway ready to deploy

--- build-firmware-f17gw.sh
+++ build-firmware-f17gw.sh
@@ -6,7 +6,7 @@ VERSION=18.06.1
 PLATFORM=ar71xx
 TYPE=generic
 PROFILE=tl-wdr4300-v1
-PACKAGES="luci-app-wireguard luci-proto-wireguard wireguard-tools prometheus-node-exporter-lua ddns-scripts luci-app-ddns -dnsmasq dnsmasq-full luci-proto-ipv6 tcpdump-mini curl ip-full uhttpd luci-mod-admin-full luci-theme-bootstrap libiwinfo-lua -wpa-supplicant -wpa-supplicant-mini -wpad-mini wpad -hostapd -hostapd-common -odhcpd-ipv6only curl ca-certificates libustream-openssl watchcat luci-app-watchcat"
+PACKAGES="luci-app-firewall luci-app-wireguard luci-proto-wireguard wireguard-tools prometheus-node-exporter-lua ddns-scripts luci-app-ddns -dnsmasq dnsmasq-full luci-proto-ipv6 tcpdump-mini curl ip-full uhttpd luci-mod-admin-full luci-theme-bootstrap libiwinfo-lua -wpa-supplicant -wpa-supplicant-mini -wpad-mini wpad -hostapd -hostapd-common -odhcpd-ipv6only curl ca-certificates libustream-openssl watchcat luci-app-watchcat"
 
 #if [ "${VERSION}" = "snapshot" ] 
 #  then wget -O /tmp/imagebuilder.tar.xz https://downloads.openwrt.org/snapshots/targets/${PLATFORM}/${TYPE}/openwrt-imagebuilder-${PLATFORM}-${TYPE}.Linux-x86_64.tar.xz
